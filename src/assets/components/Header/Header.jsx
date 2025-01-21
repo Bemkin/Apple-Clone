120a16aea8bc72a61856971b300515610609d1b8
Bemnet <bemnetkibret4@gmail.com>
2025-01-21 23:22:33 +0300

    header fix

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,36 +1,80 @@
-import React from 'react'
-import icon from '../../../../../apple-responsive-replica-html-css-mar-2020/images/icons/logo-sm.png'
-import cart from '../../../../../apple-responsive-replica-html-css-mar-2020/images/icons/cart-sm.png'
-import search from '../../../../../apple-responsive-replica-html-css-mar-2020/images/icons/search-icon-sm.png'
-import { Link } from 'react-router-dom'
+// src/assets/components/Header/Header.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import icon from '../images/icons/logo-sm.png';
+import cart from '../images/icons/cart-sm.png';
+import search from '../images/icons/search-icon-sm.png';
 
 export default function Header() {
   return (
     <div>
-          <div className="nav-wrapper fixed-top">
-		<div className="container">
-			<nav className="navbar navbar-toggleable-sm navbar-expand-md">
-			    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-			        ☰
-			    </button>
-			    <Link className="navbar-brand mx-auto" to='/Apple-Clone/'><img src={icon}/></Link>
+      <div className="nav-wrapper fixed-top">
+        <div className="container">
+          <nav className="navbar navbar-toggleable-sm navbar-expand-md">
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-toggle="collapse"
+              data-target=".navbar-collapse"
+            >
+              ☰
+            </button>
+            <Link className="navbar-brand mx-auto" to="/Apple-Clone/">
+              <img src={icon} alt="Logo" />
+            </Link>
 
-			    <div className="navbar-collapse collapse">
-			        <ul className="navbar-nav nav-justified w-100 nav-fill">
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/mac">Mac</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/iphones">iphone</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/ipad">ipad</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/watch">watch</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/tv">tv</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/music">Music</Link></li>
-						<li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/support">Support</Link></li>
-						<li className="nav-item" ><Link className="nav-link js-scroll-trigger" to="/search/"><img src={search}/></Link></li>
-						<li className="nav-item"><Link  className="nav-link js-scroll-trigger" to="/cart/"><img src={cart}/></Link></li>
-			        </ul>
-			    </div>
-			</nav>
-		</div>
-	</div>
+            <div className="navbar-collapse collapse">
+              <ul className="navbar-nav nav-justified w-100 nav-fill">
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/mac">
+                    Mac
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/iphones">
+                    iPhone
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/ipad">
+                    iPad
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/watch">
+                    Watch
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/tv">
+                    TV
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/music">
+                    Music
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/support">
+                    Support
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/search/">
+                    <img src={search} alt="Search" />
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link js-scroll-trigger" to="/cart/">
+                    <img src={cart} alt="Cart" />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
